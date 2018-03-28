adb4267a6bb00200acdb2b59656f0f8f1816984a
fodhil zidane <fodhilzidane@gmail.com>
2018-03-27 23:40:34 -0400

    commit

--- School/Controllers/StudentController.cs
+++ School/Controllers/StudentController.cs
@@ -9,24 +9,8 @@ namespace School.Controllers
 {
     public class StudentController : Controller
     {
-        public static List<Student> Students = new List<Student>
-                    {
-                        new Student { StudentId = 1, LastName = "Halpert", FirstName = "Jim" },
-                        new Student { StudentId = 2, LastName = "Beesly", FirstName = "Pam" },
-                        new Student { StudentId = 3, LastName = "Scott", FirstName = "Michael"},
-                        new Student { StudentId = 4, LastName = "Schrute", FirstName = "Dwight" },
-                        new Student { StudentId = 5, LastName = "Martin", FirstName = "Angela" },
-                        new Student { StudentId = 6, LastName = "Bernard", FirstName = "Andy" },
-                        new Student { StudentId = 7, LastName = "Malone", FirstName = "Kevin" },
-                        new Student { StudentId = 8, LastName = "Kapoor", FirstName = "Kelly" },
-                        new Student { StudentId = 9, LastName = "Palmer", FirstName = "Meredith" },
-                        new Student { StudentId = 10, LastName = "Flenderson", FirstName = "Toby" },
-                        new Student { StudentId = 11, LastName = "Hudson", FirstName = "Stanley" },
-                        new Student { StudentId = 12, LastName = "Bratton", FirstName = "Creed" },
-                        new Student { StudentId = 13, LastName = "Vance", FirstName = "Phyllis" },
-                        new Student { StudentId = 14, LastName = "Howard", FirstName = "Ryan" },
-                        new Student { StudentId = 15, LastName = "Philbin", FirstName = "Darryl" }
-                    };
+        public static List<Student> Students = new List<Student> { };
+                    
 
         public ActionResult Index()
         {
